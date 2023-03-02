85fe5cf606487fac49552035ad3610f2a32db8fb
Patrick Lauher <plauher14@gmail.com>
2023-03-02 16:08:53 -0600

    edit twoNumberSum

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -1,5 +1,5 @@
-// write a function that takes in a non-empty array of distinct integers and an integer representing a target SubmitEvent. If any 2 numbers in the
-// input array sum up to the target sum, the function should return then in an array, in any order. If no 2 numbers sum up to the target sum,
+// write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any 2 numbers in the
+// input array sum up to the target sum, the function should return them in an array, in any order. If no 2 numbers sum up to the target sum,
 // the function should return an empty Array.
 // Note that the target sum has to be obtained by summing 2 different integers in the array; you can't add a single integer to itslef in order
 // obtain the target sum.
